2e99995d3c09f56d55af559148eb19966d5ba921
Nikhil Barthwal <nikhilbarthwal@geico.com>
2026-08-15 08:15:00 -0700

    .

--- Library/graph.h
+++ Library/graph.h
@@ -48,20 +48,28 @@ public:
 
     class Data
     {
+    protected:
+        unique_ptr<Segment>& data;
+
     public:
-        const float MinX;
-        const float MaxX;
         const string Color;
         const string Label;
         const bool Point;
 
-        Data(float min_x, float max_x, const string& color,
-             const string& label, bool point);
+        Data(const string& color, const string& label, bool point);
+        [[nodiscard]] const unique_ptr<Segment>& Value(float p);
 
-        [[nodiscard]] virtual const unique_ptr<Segment>& Value(float p) const = 0;
+        virtual void Update(float) = 0;
         virtual ~Data() = default;
     };
 
+private:
+    vector<unique_ptr<Data>>& data;
+
+protected:
+    [[nodiscard]] virtual string Title(float parameter) const = 0;
+
+public:
     const bool Slider;
     const string WindowTitle;
     const float MinP;
@@ -72,12 +80,10 @@ public:
     const float MaxY;
     const std::vector<std::unique_ptr<Data>> Segments;
 
-    Graph(const string& title, bool slider, float min_p, float max_p, float min_x,
-          float max_x, float min_y, float max_y);
+    Graph(const string& title, bool slider, float min_p, float max_p,
+          float min_x, float max_x, float min_y, float max_y,
+          const vector<unique_ptr<Data>>&);
 
-    [[nodiscard]] virtual const vector<unique_ptr<Data>>& DataSet() const = 0;
-
-    [[nodiscard]] virtual string Title(float parameter) const = 0;
     GRAPH_API void Show(float parameter);
     GRAPH_API void Plot();
     virtual ~Graph() = default;
@@ -97,9 +103,9 @@ public:
 
     Histogram(const string& title, const bool slider, const float min_p,
               const float max_p, const float min_x, const float max_x,
-              const float min_y, const float max_y) : Slider(slider), WindowTitle(
-              title), MinP(min_p), MaxP(max_p), MinX(min_x), MaxX(max_x),
-              MinY(min_y), MaxY(max_y) { }
+              const float min_y, const float max_y):
+                  Slider(slider), WindowTitle(title), MinP(min_p), MaxP(max_p),
+                  MinX(min_x), MaxX(max_x), MinY(min_y), MaxY(max_y) { }
 
     class Data
     {
